e3b50cf6ad13eda32e73f37006506fc89f242db9
DrinaLuburich <drina_lf93@hotmail.com>
2022-12-04 23:16:35 +0100

    add lombok

--- src/main/java/com/ironhack/crm/model/Account.java
+++ src/main/java/com/ironhack/crm/model/Account.java
@@ -1,10 +1,14 @@
 package com.ironhack.crm.model;
-
+import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
 public class Account {
-
     private String companyName;
     private String industry;
     private int employees;
