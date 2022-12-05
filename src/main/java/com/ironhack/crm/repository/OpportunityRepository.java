987407b90ab3cbcb43f4668ca58875c5bdca3e83
Daniel Lopez <74362158+dwolfez@users.noreply.github.com>
2022-12-05 17:43:04 +0100

    Update OpportunityRepository.java
    updating recently added query before merge

--- src/main/java/com/ironhack/crm/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/crm/repository/OpportunityRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
-
+  Opportunity findOpportunityByOpportunityId(Long id);
 }
