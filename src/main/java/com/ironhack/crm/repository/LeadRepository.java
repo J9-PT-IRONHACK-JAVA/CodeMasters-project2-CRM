4caebed323d206cf43a2975365194268fea7fa9c
dwolfez <daniel.lj84@gmail.com>
2022-12-07 01:20:47 +0100

    fixed opportunity creation and status change + added search lead by name :smile:

--- src/main/java/com/ironhack/crm/repository/LeadRepository.java
+++ src/main/java/com/ironhack/crm/repository/LeadRepository.java
@@ -1,14 +1,18 @@
 package com.ironhack.crm.repository;
 
 import com.ironhack.crm.model.Lead;
-import com.ironhack.crm.services.LeadService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LeadRepository extends JpaRepository<Lead, Long> {
 
     Lead findLeadByLeadId(Long id);
-    Lead findLeadByNameEquals(String name);
+
+    List<Lead> findLeadByNameContaining(String name);
+
+    void deleteLeadByLeadId(Long id);
 
 }
